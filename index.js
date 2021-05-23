4a2905a07583c4aaf621714133e2c5235bffd25c
Jamal Gelin <gelin.jamal@gmail.com>
2021-05-23 14:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your solution in this file!
 function updateDriverWithKeyAndValue(driver, key, value) {
-  
+    driver = {
+      
+    }
 }
 
 function destructivelyUpdateDriverWithKeyAndValue() {
